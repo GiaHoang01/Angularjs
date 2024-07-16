a00ff75b927322218b822c521677072dd0e4db9f
GiaHoang01 <gianh1906@gmail.com>
2024-07-16 14:56:55 +0700

    commit

--- bai6/Bai7/Handle.js
+++ bai6/Bai7/Handle.js
@@ -6,25 +6,47 @@
 
     function Handle($scope,$location) {
         var list = [
-            { Name: "Angularjs", Like: 10, Dislike: 0,status:true },
-            { Name: "Nodejs", Like: 10, Dislike: 0, status: true},
-            { Name: "Reactionjs", Like: 10, Dislike: 0, status: false },
-            { Name: "TypeScript", Like: 10, Dislike: 0, status: true},
+            { name: "Angularjs", like: 10, dislike: 2,status:true },
+            { name: "Bodejs", like: 9, dislike: 3, status: true},
+            { name: "Ceactionjs", like: 8, dislike: 4, status: false },
+            { name: "DypeScript", like: 7, dislike: 5, status: true},
         ];
         $scope.listLanguages = list;
         $scope.increaseLike = function (lesson) {
-            lesson.Like++;
+            lesson.like++;
         }
-        $scope.increaseDisLike = function (lesson) {
+        $scope.increaseDislike = function (lesson) {
             lesson.Dislike++;
         }
-        app.filter("status", function () {
-            return function (input) {
-                if (input = true)
-                    return "kích hoạt";
-                else
-                    return "khóa";
+
+        $scope.sortColumn = 'name';
+        $scope.reverse = false;
+
+        $scope.sortData = function (column) {
+            if ($scope.sortColumn==column)
+                $scope.reverse = !$scope.reverse;
+            else
+                $scope.reverse = false;
+                $scope.sortColumn = column;
+        }
+
+        $scope.getSortClass = function (column)
+        {
+            if ($scope.sortColumn == column) {
+                return $scope.reverse ? 'arrow-up' : 'arrow-down';
             }
-        });
+            return '';
+        }
+       
     }
+
+    app.filter("status", function () {
+        return function (input) {
+            if (input = true)
+                return "kích hoạt";
+            else
+                return "khóa";
+        }
+    });
+
 })();
