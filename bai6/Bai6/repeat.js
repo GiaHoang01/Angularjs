9333a228b7f1c3140bf25e415198359f0c051e24
GiaHoang01 <gianh1906@gmail.com>
2024-07-16 13:48:49 +0700

    Add project files.

---
+++ bai6/Bai6/repeat.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module("app", []);
+    app.controller('repeatController', repeatController);
+
+    app.$inject = ['$scope','$location'];
+
+    function repeatController($scope,$location) {
+        var emp = [
+            { lastName: "Ben", firstName: "Nguyen", gender: "male" },
+            { lastName: "Bo", firstName: "Tran", gender: "female" },
+            { lastName: "Buong", firstName: "Nguyen", gender: "male" },
+        ];
+        $scope.emp = emp;
+    }
+})();
---
+++ bai6/Bai7/Handle.js
@@ -0,0 +1,30 @@
+﻿(function () {
+    'use strict';
+    var app = angular.module("app", []);
+    app.controller("Handle", Handle);
+    app.$inject = ['$scope','$location'];
+
+    function Handle($scope,$location) {
+        var list = [
+            { Name: "Angularjs", Like: 10, Dislike: 0,status:true },
+            { Name: "Nodejs", Like: 10, Dislike: 0, status: true},
+            { Name: "Reactionjs", Like: 10, Dislike: 0, status: false },
+            { Name: "TypeScript", Like: 10, Dislike: 0, status: true},
+        ];
+        $scope.listLanguages = list;
+        $scope.increaseLike = function (lesson) {
+            lesson.Like++;
+        }
+        $scope.increaseDisLike = function (lesson) {
+            lesson.Dislike++;
+        }
+        app.filter("status", function () {
+            return function (input) {
+                if (input = true)
+                    return "kích hoạt";
+                else
+                    return "khóa";
+            }
+        });
+    }
+})();
